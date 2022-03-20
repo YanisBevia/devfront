a7cf3c3b69a08a0d0fe94f68363c321f6648e0f1
Yann XY <yannatkk@gmail.com>
2022-03-21 00:37:15 +0100

    create skills service

--- skills-trade/src/app/models/skills.model.ts
+++ skills-trade/src/app/models/skills.model.ts
@@ -1,6 +1,6 @@
 export class Skills {
     id!: number;
     category!: string;
-    items!: [];
+    items!: [string, string, string];
     imagePath!: string;
 }
--- skills-trade/src/app/services/skills.service.ts
+++ skills-trade/src/app/services/skills.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Skills } from '../models/skills.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillsService {
 
-  
+  skills: Skills[] = [
+    {
+      id: 1,
+      category: 'Informatique',
+      items: ["Développeur Web", "Développeur Mobile", "DevOps"],
+      imagePath: "assets/image-test.jpg",
+    },
+  ];
+
+  getAllSkills(): Skills[] {
+    return this.skills;
+  }
+
+  getSkillById(skillId: number): Skills {
+    const skill = this.skills.find(skill => skill.id === skillId);
+
+    if (!skill) {
+      throw new Error('Skill not found!');
+    } else {
+      return skill;
+    } 
+  }
 }
